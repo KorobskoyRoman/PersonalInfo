4462e2c2580c17c5fd17dc97aade8cebd86ce685
KorobskoyRoman <shopone023@gmail.com>
2022-10-24 17:28:47 +0300

    Initial Commit

---
+++ PersonalInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalInfo
+//
+//  Created by Roman Korobskoy on 24.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
