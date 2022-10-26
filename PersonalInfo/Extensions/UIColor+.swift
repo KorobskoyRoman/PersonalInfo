dcbe6bb6edba4a8201ac2a8935d9f3ec14ac67b5
KorobskoyRoman <shopone023@gmail.com>
2022-10-26 21:56:43 +0300

    Added UIColor extension

---
+++ PersonalInfo/Extensions/UIColor+.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+.swift
+//  PersonalInfo
+//
+//  Created by Roman Korobskoy on 24.10.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static func mainBackground() -> UIColor {
+        return #colorLiteral(red: 0.9098039216, green: 0.9176470588, blue: 0.9254901961, alpha: 1)
+    }
+}
