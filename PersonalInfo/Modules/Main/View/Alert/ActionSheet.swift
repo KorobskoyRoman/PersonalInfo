1ccd8e6cd9c0c64a87e7e4c003ef343a9b5e9a75
KorobskoyRoman <shopone023@gmail.com>
2022-10-27 23:56:30 +0300

    Added action sheet

---
+++ PersonalInfo/Modules/Main/View/Alert/ActionSheet.swift
@@ -0,0 +1,26 @@
+//
+//  ActionSheet.swift
+//  PersonalInfo
+//
+//  Created by Roman Korobskoy on 27.10.2022.
+//
+
+import UIKit
+
+final class ActionSheet {
+    static func showActionsheet(viewController: UIViewController,
+                                title: String,
+                                message: String,
+                                completion: @escaping (UIAlertAction) -> Void) {
+        let alertViewController = UIAlertController(title: title,
+                                                    message: message,
+                                                    preferredStyle: .actionSheet)
+        let actionOk = UIAlertAction(title: "Удалить", style: .destructive, handler: completion)
+        let actionCancel = UIAlertAction(title: "Отмена", style: .cancel)
+
+        alertViewController.addAction(actionOk)
+        alertViewController.addAction(actionCancel)
+
+        viewController.present(alertViewController, animated: true, completion: nil)
+    }
+}
