47d58ea5e87c8f15eb05af0490ad50cc9be95460
KorobskoyRoman <shopone023@gmail.com>
2022-10-28 00:05:28 +0300

    Updated view model

--- PersonalInfo/Modules/Main/MainViewModel.swift
+++ PersonalInfo/Modules/Main/MainViewModel.swift
@@ -11,12 +11,45 @@ import RxCocoa
 protocol MainViewModelType {
     var childs: BehaviorRelay<[Child]> { get set }
     func getChild(at index: Int) -> Child
+    func deleteChild(at child: Child)
+    func changeValues(newValue: String,
+                      indexPath: IndexPath,
+                      type: TextFieldType)
 }
 
 final class MainViewModel: MainViewModelType {
-    var childs = BehaviorRelay<[Child]>(value: [Child(name: "sda", age: "233")])
+    var childs = BehaviorRelay<[Child]>(value: [])
 
     func getChild(at index: Int) -> Child {
-        childs.value[index]
+        guard !childs.value.isEmpty else { return .emptyChild }
+        return childs.value[index]
+    }
+
+    func deleteChild(at child: Child) {
+        guard let index = childs.value
+            .firstIndex(where: { $0.id == child.id }) else { return }
+        var value = childs.value
+        value.remove(at: index)
+        childs.accept(value)
+    }
+
+    func changeValues(newValue: String,
+                      indexPath: IndexPath,
+                      type: TextFieldType) {
+        print(childs.value)
+        var value = getChild(at: indexPath.row)
+        deleteChild(at: value)
+        let oldValue = childs.value
+
+        switch type {
+        case .name:
+            value.name = newValue
+            childs.accept(oldValue + [value])
+            break
+        case .age:
+            value.age = newValue
+            childs.accept(oldValue + [value])
+            break
+        }
     }
 }
